4f5fb0149e6d35764b54b56f5e6a18df8ee49a13
awetoga <awetogaabraham1@gmail.com>
2023-04-28 03:52:04 +0000

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  )
+}
+
+export default App
